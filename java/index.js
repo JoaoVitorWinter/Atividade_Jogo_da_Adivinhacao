3a57ac7150d40e81c2af0e24a7cda37dc374b685
JoaoVitorWinter <joao_v_winter@estudante.sc.senai.br>
2022-11-29 08:59:51 -0300

    Terminado HTML do jogo

--- java/index.js
+++ java/index.js
@@ -1,12 +1,29 @@
 let nome
 let modo = 0
+let numRandom
 
 function setarNome () {
     nome = document.getElementById("nome").value
-    document.getElementById("nome").disabled = true
-    console.log(nome)
+    if(nome != ""){
+        document.getElementById("nome").disabled = true
+        document.getElementById("insano").removeAttribute("disabled")
+        document.getElementById("dificil").removeAttribute("disabled")
+        document.getElementById("medio").removeAttribute("disabled")
+        document.getElementById("facil").removeAttribute("disabled")
+        document.getElementById("noob").removeAttribute("disabled")
+    }
 }
 
 function setarModo (num) {
     modo = num
+    document.getElementById("insano").disabled = true
+    document.getElementById("dificil").disabled = true
+    document.getElementById("medio").disabled = true
+    document.getElementById("facil").disabled = true
+    document.getElementById("noob").disabled = true
+    jogar()
+}
+
+function jogar () {
+    numRandom = parseInt(Math.random()*99 + 1)
 }
